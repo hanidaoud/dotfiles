edd6935646b1e841e0dd7ab4679ac12107dfc6cc
hanidaoud <hanidaoudgt@gmail.com>
2022-08-25 20:40:18 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+DIR0=`pwd`
+DIR1="$HOME"
+PROG="bspwm sxhkd polybar kitty picom rofi"
+PROG_NO_CONF="flameshot pulsemixer xbacklight nitrogen xorg-server xorg-xinit sddm"
+
+sudo pacman -Syy
+sudo pacman -S $PROG $PROG_NO_CONF
+
+mkdir $DIR1/.config_backups
+
+for i in $PROG
+do
+	cp -vr $DIR1/.config/$i $DIR1/.config_backups
+	rm -vrf $DIR1/.config/$i
+done
+
+cp -vr $DIR0/dotfiles/* $DIR1/.config/
+
+echo -ne '
+# Aliases
+alias ls="ls --color=auto -alh"
+alias logout="bspc quit"
+' >> $DIR1/.bashrc
+
+echo -ne '
+# Aliases
+alias ls="ls --color=auto -alh"
+alias logout="bspc quit"
+' >> $DIR1/.zshrc
