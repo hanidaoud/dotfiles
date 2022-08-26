5ceb6b7e815f0eab9d6febe5912d02013cb166b5
hanidaoud <hanidaoudgt@gmail.com>
2022-08-26 18:34:20 +0100

    removed .Xresources

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DIR0=`pwd`
 DIR1="$HOME"
 PROG="bspwm sxhkd polybar kitty picom rofi"
-PROG_NO_CONF="flameshot pulsemixer xorg-xbacklight nitrogen xorg-server xorg-xinit sddm wget adwaita-icon-theme ntfs-3g"
+PROG_NO_CONF="flameshot pulsemixer xorg-server xorg-xinit xorg-xbacklight xorg-xsetroot nitrogen sddm wget adwaita-icon-theme ntfs-3g"
 
 sudo pacman -Syy noto-fonts --noconfirm --needed --asdeps
 sudo pacman -S $PROG $PROG_NO_CONF --noconfirm --needed
@@ -72,8 +72,6 @@ fi
 echo -ne '
 [Icon Theme]
 Inherits=Adwaita' >> $DIR1/.icons/index.theme
-
-echo "Xcursor.theme: Adwaita" >> $DIR1/.Xresources
 
 while true
 do
