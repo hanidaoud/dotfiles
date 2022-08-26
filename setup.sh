c2da07f4f0608b11a23c42fccaea8f0b27195546
hanidaoud <hanidaoudgt@gmail.com>
2022-08-26 17:24:35 +0100

    cursor theme

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DIR0=`pwd`
 DIR1="$HOME"
 PROG="bspwm sxhkd polybar kitty picom rofi"
-PROG_NO_CONF="flameshot pulsemixer xorg-xbacklight nitrogen xorg-server xorg-xinit sddm wget"
+PROG_NO_CONF="flameshot pulsemixer xorg-xbacklight nitrogen xorg-server xorg-xinit sddm wget adwaita-icon-theme"
 
 sudo pacman -Syy
 sudo pacman -S $PROG $PROG_NO_CONF
@@ -59,3 +59,18 @@ echo -ne '
 alias ls="ls --color=auto -alh"
 alias logout="bspc quit"
 ' >> $DIR1/.zshrc
+
+if [ ! -d $DIR1/.icons ]
+then
+	mkdir $DIR1/.icons
+else
+	mkdir -p $DIR1/.icons/.backup
+	cp $DIR1/.icons/index.theme $DIR1/.icons/.backup
+	rm $DIR1/.icons/index.theme
+fi
+
+echo -ne '
+[Icon Theme]
+Inherits=Adwaita' >> $DIR1/.icons/index.theme
+
+
