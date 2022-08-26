865648d475d45ac65a77e244df112643aec0bb75
hanidaoud <hanidaoudgt@gmail.com>
2022-08-26 14:25:19 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DIR0=`pwd`
 DIR1="$HOME"
 PROG="bspwm sxhkd polybar kitty picom rofi"
-PROG_NO_CONF="flameshot pulsemixer xorg-xbacklight nitrogen xorg-server xorg-xinit sddm"
+PROG_NO_CONF="flameshot pulsemixer xorg-xbacklight nitrogen xorg-server xorg-xinit sddm wget"
 
 sudo pacman -Syy
 sudo pacman -S $PROG $PROG_NO_CONF
@@ -20,6 +20,29 @@ then
 else
 	mkdir $DIR1/.config
 fi
+
+dload()
+{
+	if ! wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip"
+	then
+		dload
+	fi
+}
+dload
+
+if [ ! -d "$DIR1/.fonts" ]
+then
+	mkdir "$DIR1/.fonts"
+fi
+
+cd "$DIR1/.fonts"
+mkdir Hack && cd Hack
+unzip -x $DIR0/Hack.zip
+cd $DIR0
+rm Hack.zip
+
+fc-cache -vf
+
 
 cp -vr $DIR0/dotfiles/* $DIR1/.config/
 
